f3fda1a10860b6bd1fb3e766f8ed817fbf0f1ddc
linshuc <149919876+linshuc@users.noreply.github.com>
2023-11-21 21:05:54 +0800

    Update 2.3.cpp

--- 实验2/2.3.cpp
+++ 实验2/2.3.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 int main() {
-	cout << "请输入三角形三条边长" << "/n"<<"三角形的周长为"<<a+b+c<<endl;
+	cout << "请输入三角形三条边长" << endl;
 	int a, b, c;
 	cin >> a >> b >> c;
 	int s;
 	if (((a + b) > c)&&((a+c)>b)&&((b+c)>a)) {
+		cout<<"三角形的周长为"<<a+b+c<<endl;
 		if ((a = b)||(a=c)||(b=c)) { cout << "此三角形是等腰三角形"; }
 		else { cout << "此三角形不是等边三角形"; }
 	}
