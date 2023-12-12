d7a6e2e3e13b52c7543dccf62307738c08b1da52
linshuc <149919876+linshuc@users.noreply.github.com>
2023-12-12 22:12:56 +0800

    Update 4-2.1.3.cpp

--- 实验34/实验4/4-2.1.3.cpp
+++ 实验34/实验4/4-2.1.3.cpp
@@ -3,11 +3,11 @@ using namespace std;
 void f(char* st, int i)
 {
     st[i] = '\0';
-    cout << st;   // printf(¡°%s\n¡±,st);
+    cout << st;   // printf(Â¡Â°%s\nÂ¡Â±,st);
     if (i > 1) f(st, i - 1);
 }
 void main()
 {
     char st[] = "abcd";
-    f(st, 4);//½á¹ûÊÇabcdabcaba
+    f(st, 4);//abcdabcaba
 }
